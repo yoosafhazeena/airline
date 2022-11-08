5dc750859c19be16c6f86859ec24a1aa5102144c
Hazeena Yoosaf <114492454+yoosafhazeena@users.noreply.github.com>
2022-11-08 06:24:54 +0530

    seperate js file

---
+++ flights/templates/flights/counter.js
@@ -0,0 +1,9 @@
+let counter = 0;
+function count(){
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('button').onclick = count;
+})
